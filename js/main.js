862314acc30380f76384df9d60d31035be25393c
Coding <coding@JobTrainworks.org>
2016-09-15 14:03:14 -0700

    Adjust fade animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $('.scoop').on('mouseenter', function(){
   var thisScoop = $(this).attr('value');
+  console.log(thisScoop);
   $('.desc').hide();
   $('.flavor-desc-' + thisScoop).fadeIn();
 });
 
-$('.scoop').on('mouseleave', function() {
+$('.scoop').on('click', function(){
   var thisScoop = $(this).attr('value');
-  $('.desc').fadeOut(200);
-})
+  console.log(thisScoop);
+  $('.desc').hide();
+  $('.flavor-desc-' + thisScoop).show();
+});
+
+// $('.scoop').on('mouseleave', function() {
+//   var thisScoop = $(this).attr('value');
+//   $('.desc').fadeOut(200);
+// })
